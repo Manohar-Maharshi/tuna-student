564dc85c9d5376913e81b52383685cfbdab442ba
Manohar-Maharshi <netwitch101@gmail.com>
2022-01-31 15:24:58 +0530

    ntg

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,15 +1,16 @@
 <script>
-	import "../app.css";
-	import '@fontsource/inter';
-
-	import NavBar from '$lib/components/NavBar.svelte'
-	import Footer from '$lib/components/Footer.svelte'
-
+  import "../app.css";
+  import "@fontsource/inter";
+  import NavBar from "$lib/components/NavBar.svelte";
+  import Footer from "$lib/components/Footer.svelte";
 </script>
+
 <svelte:head>
-	<script defer src="https://unpkg.com/@themesberg/flowbite@1.3.0/dist/flowbite.bundle.js"></script>
+  <script
+    defer
+    src="https://unpkg.com/@themesberg/flowbite@1.3.0/dist/flowbite.bundle.js"></script>
 </svelte:head>
 
 <NavBar />
-<slot></slot>
+<slot />
 <Footer />
